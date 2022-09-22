514d07ded03ad6d585c3e4809d8fd1bf39ddabd2
AnkitGupta87 <59764028+AnkitGupta87@users.noreply.github.com>
2022-09-22 18:04:03 +0530

    Add files via upload
    Day - 9 ( Assignments )

---
+++ Day-9/Assignment-2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_9_Assignment2
+{
+    class Circle
+    {
+        float radius;
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+        public void Perimeter()
+        {
+            Console.WriteLine("Perimeter of Circle is : "+2 * 3.14 * radius);
+        }
+        public void Area()
+        {
+            Console.WriteLine("Area of Circle is : " + 3.14 * radius * radius);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Radius of Circle : ");
+            int radius;
+            radius = Convert.ToInt32(Console.ReadLine());
+            Circle mycircle = new Circle(radius);
+            mycircle.Perimeter();
+            mycircle.Area();
+
+            Console.ReadKey();
+        }
+    }
+}
