b283dd9ab633688e2aae09363d8188117539d7d1
AnkitGupta87 <ankitgupta208999@gmail.com>
2022-10-19 21:32:20 +0530

    Assignment-1 19-10-22

---
+++ CSharp_Assignment_19_10_22_Q_1/CSharp_Assignment_19_10_22/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CSharp_Assignment_19_10_22
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string file = @"C:\Users\ankit.gupta\OneDrive - Entain Group\Documents\first.txt";
+            Console.WriteLine(File.ReadAllText(file));
+            string str = @"C:\Users\ankit.gupta\OneDrive - Entain Group\Documents\second.txt";
+            await Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine(File.ReadAllText(str));
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
